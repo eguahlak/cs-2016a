399b96dcc645b9a101b09694e0fdbd8bc4b3d5e9
Anders Kalhauge <anders@kalhauge.dk>
2016-02-29 15:56:59 +0100

    Properties

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 #endif
 using System;
+using Model;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,35 @@ namespace Hello {
       static int x;
       static Int32 y;
       static void Main(string[] args) {
-        
-        System.Console.WriteLine("Hello 4th semester");
+        Person sonja = new Person(7, "Sonja", "Olsen");
+        Console.WriteLine("ID: "+sonja.Id);
+        Console.WriteLine("FN: "+sonja.FirstName);
+        Console.WriteLine("LN: "+sonja.LastName);
+        Console.WriteLine(sonja);
+        sonja[1] = "Rufus";
+        Console.WriteLine(sonja);
+        sonja[100] = "Felix";
+        Console.WriteLine(sonja);
+        sonja[0] = "Lotte";
+        Console.WriteLine(sonja);
+
+
+        // sonja.Id = 8;
+        sonja.LastName = null;
+        Console.WriteLine("LN: "+sonja.LastName);
+        try {
+          sonja.LastName = "F";
+          Console.WriteLine("LN: "+sonja.LastName);
+          }
+        catch (ArgumentOutOfRangeException) {
+        //catch (ArgumentOutOfRangeException ex) {
+          Console.WriteLine("-- arg exp --");
+          }
+        catch {
+          Console.WriteLine("-- hovsa --");
+          }
+        Console.WriteLine();
+        Console.WriteLine("Hello 4th semester");
         //Console.ReadKey();
         }
       }
---
+++ Model/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model {
+  public class Person {
+    public int Id { get; private set; }
+    public string FirstName { get; set; }
+    private string lastName;
+    private long value;
+    private string pets = "Ninus";
+
+    public override string ToString() {
+      return ""+Id+") "+Name+" "+pets;
+      }
+
+    public string this[int index] {
+      get { return pets.Split(' ')[index]; }
+      set {
+        string[] parts = pets.Split(' ');
+        if (index < parts.Length) {
+          parts[index] = value;
+          pets = string.Join(" ", parts);
+          }
+        else {
+          pets += " "+value;
+          }
+        }
+      }
+
+    public string LastName {
+      get { return lastName; }
+      set {
+        if (value == null) value = "Unknown";
+        if (value.Length < 2)
+            throw new ArgumentOutOfRangeException();
+        lastName = value;
+        }
+      }
+
+    public decimal Value {
+      get { return value/100m; }
+      set { this.value = (long)(100*value); }
+      }
+
+    public Person(int id, string firstName, string lastName) {
+      Id = id;
+      FirstName = firstName;
+      // this.lastName = lastName;
+      LastName = lastName;
+      }
+
+    public string Name {
+      get { return FirstName+" "+LastName; }
+      set {
+        string[] parts = value.Split(' ');
+        if (parts.Length < 2) return;
+        FirstName = parts[0];
+        LastName = parts[1];
+        }
+      }
+    
+    }
+  }
