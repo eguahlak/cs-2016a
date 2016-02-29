a4b91e6c7a6c2967b43f466cff898ce1b4a372e7
Anders Kalhauge <anders@kalhauge.dk>
2016-02-29 13:10:52 +0100

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿#if DEBUG
+using System.Linq;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello {
+  namespace World {
+    class Program {
+      static int x;
+      static Int32 y;
+      static void Main(string[] args) {
+        
+        System.Console.WriteLine("Hello 4th semester");
+        //Console.ReadKey();
+        }
+      }
+    }
+  }
