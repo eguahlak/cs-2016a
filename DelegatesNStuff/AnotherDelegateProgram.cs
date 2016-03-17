2bb22c40a212e367a75cc20fb609d18ea5ef0761
Anders Kalhauge <anders@kalhauge.dk>
2016-03-17 09:27:55 +0100

    Before 05

---
+++ DelegatesNStuff/AnotherDelegateProgram.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegatesNStuff {
+
+  delegate string Worker(string text);
+
+  class AnotherDelegateProgram {
+    
+    public static string WorkLongTime(string text) {
+      Console.WriteLine("starting long method...");
+      Thread.Sleep(2000);
+      Console.WriteLine("The text "+text);
+      return text+" processed";
+      }
+
+    public static string WorkLessTime(string text) {
+      Console.WriteLine("starting less long method...");
+      Thread.Sleep(500);
+      Console.WriteLine("Less time text "+text);
+      return text+" processed quickly";
+      }
+
+    public static void CallBack(IAsyncResult result) {
+      Console.WriteLine("Jeg er færdiiii...");
+      AsyncResult ar = (AsyncResult)result;
+      Worker w = (Worker)ar.AsyncDelegate;
+      string t = w.EndInvoke(result);
+      Console.WriteLine("### "+t);
+      } 
+    
+    static void Main() {
+      Worker w1 = WorkLongTime;
+      Worker w2 = WorkLessTime;
+
+      IAsyncResult r1 = w1.BeginInvoke("Sonja", CallBack, null);
+      IAsyncResult r2 = w2.BeginInvoke("Kurt", CallBack, null);
+      Console.WriteLine("Doing something else ... ");
+      for (int i = 0; i < 100; i++) {
+        Thread.Sleep(50);
+        Console.Write(".");
+        }
+      //Console.WriteLine("$$$"+w1.EndInvoke(r1));
+      //Console.WriteLine("$$$"+w2.EndInvoke(r2));
+      }  
+
+    }
+  }
---
+++ DelegatesNStuff/YetAnotherProgram.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegatesNStuff {
+
+  class Forex {
+    public delegate void Change(string stock, double delta);
+    private Random random = new Random();
+    // private Change changeDelegate;
+
+    public event Change ChangeDelegate;// {
+      //add {
+      //  changeDelegate += value;
+      //  }
+      //remove {
+      //  changeDelegate -= value;
+      //  }
+      //}
+
+    public void Run(int count) {
+      for (int c = 0; c < count; c++) {
+        Thread.Sleep(1000);
+        double delta = random.NextDouble();
+        int stockNo = random.Next(5);
+        string stock = "Stock #"+stockNo;
+        //if (changeDelegate != null) changeDelegate(stock, delta);
+        if (ChangeDelegate != null) ChangeDelegate(stock, delta);
+        //ChangeDelegate(stock, delta);
+        }
+      }
+    }
+  
+  class YetAnotherProgram {
+    private static double deltaSum = 0.0; 
+
+    public static void RegisterStock(string stock, double delta) {
+      Console.WriteLine("Stock "+stock+" has changed "+delta);
+      }
+    
+    public static void RegisterDeltas(string stock, double delta) {
+      deltaSum += delta;
+      Console.WriteLine("Total delta until now: "+deltaSum);
+      }
+
+    public static void Haha(string a, double b) {
+      Console.WriteLine("Got you!");
+      }
+
+    static void Main() {
+      Forex forex = new Forex();
+      //forex.Run(10);
+      forex.ChangeDelegate += RegisterStock;
+      forex.ChangeDelegate += RegisterDeltas;
+      forex.ChangeDelegate += Haha;
+      forex.Run(10);
+      }
+    
+    }
+  }
