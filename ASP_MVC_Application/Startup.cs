e9b961cf62f98bfc7a81663864129bef1ff81f77
Anders Kalhauge <anders@kalhauge.dk>
2016-05-04 11:15:57 +0200

    MVC example added

---
+++ ASP_MVC_Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_MVC_Application.Startup))]
+namespace ASP_MVC_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
